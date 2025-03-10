6e29dc6534918f38978d926f767d81df65061f10
Gabriel Abarca <epoabarca@gmail.com>
2025-03-09 23:13:32 -0600

    changes in frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <>
+    <nav>
+        <div id="title">
+            <p>
+            AuthenticoTours
+            </p>
+        </div>
+        <ul>
+            <li>Activites</li>
+            <li>About</li>
+            <li>Reviews</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Header
