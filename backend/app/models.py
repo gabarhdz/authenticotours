b983363fafd3acf678fc1da36f8904da46b4d3c5
Gabriel Abarca <epoabarca@gmail.com>
2025-03-09 21:17:11 -0600

    cambios en gitignore

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,4 +10,6 @@ class Profile(models.Model):
 
 class Comment(models.Model):
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
-     
+    comment_text = models.TextField(max_length=1200)
+    def __str__(self):
+        return self.comment 
