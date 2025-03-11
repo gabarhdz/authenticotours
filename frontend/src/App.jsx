b90f4d6219862054e433dc92b0ce6aa080749474
Gabriel Abarca <epoabarca@gmail.com>
2025-03-10 20:03:29 -0600

    Creacion de la BASE de componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Header from './components/Header/Header'
+import React from 'react';
+import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Tours from './Pages/Tours/Tours';
