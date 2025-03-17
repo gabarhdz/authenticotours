648ebd50c4694ddf64eeccb2a3288278a1cc9708
ceshez <carlossanchezher10@gmail.com>
2025-03-16 19:41:30 -0600

    carrusel
    se termino la funcionalidad del carrusel en el frontend, falta conectarlo con el backend para obtener las imagenes y hacerlo responsive

--- frontend/src/components/Carrousel/Carrousel.jsx
+++ frontend/src/components/Carrousel/Carrousel.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Pagination, Navigation } from 'swiper/modules';
+import './Carrousel.css';
+
+import atvImage from '../imagenes/atv.jpg';
+import fishingImage from '../imagenes/fishing 1.jpg';
+import waterfallImage from '../imagenes/water fall 11.jpg';
+
+const images = [atvImage, fishingImage, waterfallImage];
 
 const Carrousel = () => {
   return (
-    <>
-    <div>
-        
+    <div className="carousel-container right-aligned">
+      <Swiper
+        modules={[Pagination, Navigation]}
+        spaceBetween={10}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index} className="carousel-slide">
+            <img
+              src={src}
+              alt={`Slide ${index + 1}`}
+              onError={(e) => e.target.style.display = 'none'}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Carrousel
+export default Carrousel;
