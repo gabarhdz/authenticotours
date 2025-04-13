2d7fe3bfdb4b0ffa80309940181b55aa09e48ebc
Gabriel Abarca <epoabarca@gmail.com>
2025-04-12 22:31:03 -0600

    Autenticacion con react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './Context/Auth/AuthContext.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthProvider> 
+    <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </AuthProvider>
+  ,
 )
