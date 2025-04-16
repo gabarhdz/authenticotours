5e556f3a33731e6ce1ded6cceb3cb15e58a4c075
ceshez <carlossanchezher10@gmail.com>
2025-04-15 21:43:01 -0600

    Creacion del header completamente responsive y pruebas en los comentarios
    bap bap

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
-
 import './Header.css';
+
 const Header = () => {
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+  }
+  function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+    }
   return (
     <>
-    <nav>
-        <div id="title">
-            <p>
-            AuthenticoTours
-            </p>
-        </div>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <nav id='home'>
         <ul>
-            <li>Activites</li>
-            <li>About</li>
-            <li>Reviews</li>
-            <li>Contact</li>
+            <li><a href="#">Authenticotours</a></li>
+            <li className='hideOnMobile'><a href="#">Activities</a></li>
+            <li className='hideOnMobile'><a href="#">About</a></li>
+            <li className='hideOnMobile'><a href="#">Reviews</a></li>
+            <li className='hideOnMobile'><a href="#">Contact</a></li>
+            <li className='hideOnMobile'><a href="#">EN/ES</a></li>
+            <li className='hideOnMobile'><a href="#">Log in</a></li>
+            <li className='menuButton' onClick={showSidebar}><a href="#"><span class="material-symbols-outlined">menu</span></a></li>
+        </ul>
+        <ul className="sidebar">
+            <li onClick={hideSidebar}><a href="#"><span class="material-symbols-outlined">close</span></a></li>
+            <li><a href="home">Authenticotours</a></li>
+            <li><a href="#">Activities</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Reviews</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">EN/ES</a></li>
+            <li><a href="#">Log in</a></li>
         </ul>
     </nav>
     </>
